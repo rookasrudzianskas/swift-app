195b754816db4837307c9456349086e064252202
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 15:56:44 +0300

    🔧 fix(Contents.swift): add age property and increaseAge method to HasName protocol to be able to describe objects better 🔧 fix(Contents.swift): implement increaseSpeed method in Vehicle protocol to be able to increase speed of vehicles ✨ feat(Contents.swift): add Bike struct that conforms to Vehicle protocol to be able to represent bikes 🔧 fix(Contents.swift): implement describe function to be able to describe objects that conform to Vehicle protocol 🔧 fix(SWIFT_CRASH.xcworkspace): update UserInterfaceState.xcuserstate file that was changed by Xcode

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -41,4 +41,77 @@ whiskers.jump()
 
 protocol HasName {
     var name: String { get }
+    var age: Int {get set}
+    mutating func increaseAge()
+    
 }
+
+extension HasName {
+    func describeMe() -> String {
+        "You name is \(name) and you are \(age) years old"
+    }
+    mutating func increaseAge() {
+        self.age += 1
+    }
+}
+
+struct Dog: HasName {
+    let name: String
+    var age: Int
+}
+
+var woof = Dog(name: "Whoo", age: 10)
+woof.name
+woof.age
+woof.age += 1
+woof.age
+woof.describeMe()
+
+woof.increaseAge()
+woof.age
+
+protocol Vehicle {
+    var speed: Int {get set}
+    mutating func increaseSpeed(by value: Int)
+}
+
+extension Vehicle {
+    mutating func increaseSpeed(by value :Int) {
+        self.speed = speed
+    }
+}
+
+struct Bike: Vehicle {
+    var speed: Int
+    init() {
+        self.speed = 0
+    }
+}
+
+var bike = Bike()
+bike.speed
+bike.increaseSpeed(by: 10)
+bike.speed
+
+func describe(obj: Any) {
+    if obj is Vehicle {
+        "obj confrms to vehicle protocol"
+    } else {
+        "NOT"
+    }
+}
+
+
+describe(obj: bike)
+
+
+
+
+
+
+
+
+
+
+
+
