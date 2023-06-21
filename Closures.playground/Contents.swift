f7369558a5283000041621f4b7d66316b357d34d
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 11:52:06 +0300

    🔥 chore(Closures.playground): remove commented out code and unused function declarations 🔥 chore(SWIFT_CRASH.xcworkspace): remove UserInterfaceState.xcuserstate binary file

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -24,6 +24,38 @@ customAdd(
 customAdd(
     20,
     30
-) {(lhs, rhs) in
-    lhs + rhs
+) {
+    $0 + $1
 }
+
+let ages = [30, 20, 19, 40]
+ages.sorted(by: >)
+ages.sorted(by: <)
+
+//func < (lhs: Int, rhs: Int) -> Bool {
+//
+//}
+
+func customAdd2(using function: (Int, Int) -> Int, _ lhs: Int,_ rhs: Int) -> Int {
+    function(lhs, rhs)
+}
+
+customAdd2(using: {$0 + $1 + 10}, 20, 30)
+
+func add10To(_ value: Int) -> Int {
+    value + 20
+}
+
+func add20To(_ value: Int) -> Int {
+    value + 20
+}
+
+func doAddition(on value: Int,
+                using function: (Int) -> Int
+) -> Int {
+    function(value)
+}
+
+doAddition(on: 20, using: add10To(_:))
+doAddition(on: 30, using: add20To(_:))
+
