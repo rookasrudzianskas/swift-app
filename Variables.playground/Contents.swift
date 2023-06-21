a6b954765d196ddd70043027d473850305d0a4f9
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 10:00:46 +0300

    fix

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -13,3 +13,31 @@ names.append("BAZ")
 names.append("Rimas")
 
 
+let foo = "Foo"
+var foo2 = foo
+
+foo2 = "Foo 2"
+foo
+foo2
+
+let moreNames = ["Foo", "bar"]
+
+var copy = moreNames
+copy.append("baz")
+
+copy
+
+let oldArray = NSMutableArray(
+    array: [
+        "Foo",
+        "Bar"
+    ]
+)
+
+oldArray.add("Baz")
+
+var newArray = oldArray
+
+newArray.add("Quix")
+newArray
+oldArray
