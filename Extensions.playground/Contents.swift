cb84d184e5a5eec8bd1dc5ba62b9bc3356ed2474
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 16:10:58 +0300

    🔨 refactor(Extensions.playground): add extensions to Int, Person and Car types to improve code readability and reusability 🔧 chore(Extensions.playground): enable playground to execute on source changes to improve development experience 🔥 chore(SWIFT_CRASH.xcworkspace): remove binary files from version control to reduce repository size

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -1,3 +1,56 @@
 import Foundation
 
+extension Int {
+    func plusTwo() -> Int {
+        self
+        return self + 2
+    }
+}
 
+let two = 2
+two.plusTwo()
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
+
+extension Person {
+    init(fullName: String) {
+        let components = fullName.components(separatedBy: " ")
+        self.init(firstName: components.first ?? fullName, lastName: components.last ?? fullName)
+    }
+}
+
+let person = Person(fullName: "Foo Bar")
+person.firstName
+person.lastName
+
+protocol GoesVroom {
+    var vroomValue: String {get}
+    func goVroom() -> String
+}
+
+extension GoesVroom {
+    func goVroom() -> String {
+            "\(self.vroomValue) goes vrooom"
+    }
+}
+
+struct Car {
+    let manufacturer: String
+    let model: String
+}
+
+let modelX = Car(
+    manufacturer: "Tesla", model: "X"
+)
+
+
+extension Car: GoesVroom {
+    var vroomValue: String {
+        "\(self.manufacturer) model \(self.model)"
+    }
+}
+
+modelX.goVroom()
