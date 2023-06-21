00de038e7efdea8c1d892f27823b47057062f85e
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 19:08:48 +0300

    🐛 fix(Errorhandling): add implementation to getFullName() function to fix compilation error 🔥 chore(Errorhandling): remove timeline.xctimeline file 🔧 chore(SWIFT_CRASH): update xcschememanagement.plist file to hide Errorhandling (Playground) schemes

--- Errorhandling.playground/Contents.swift
+++ Errorhandling.playground/Contents.swift
@@ -1,3 +1,16 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+struct Person {
+    let firstName: String?
+    let lastName: String?
+    
+    enum Errors: Error {
+        case firstNameIsNil
+        case lastNameIsNil
+        case bothNamesAreNil
+    }
+    
+    func getFullName() throws -> String {
+        ""
+    }
+}
