f60048f4a85c8025edc80a700542a175c0643be2
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 11:06:33 +0300

    🔨 refactor(Functions.playground): remove unused code and fix typo in function name 🔥 chore(SWIFT_CRASH.xcworkspace): remove UserInterfaceState.xcuserstate binary file from git tracking

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -5,3 +5,28 @@ func noArgumentsAndNoReturnValue() {
 }
 
 noArgumentsAndNoReturnValue()
+
+func plusTwo(value: Int) {
+    let newValue = value + 2
+    return newValue
+}
+
+plusTwo(value: 1)
+
+func newPlusTwo(value: Int) -> Int {
+    value + 2
+}
+
+newPlusTwo(value: 30)
+
+func customAdd(value1: Int, value2: Int) -> Int {
+    value1 + value2
+}
+
+let customAdded = customAdd(value1: 39, value2: 32)
+
+func customMinus(_ lhs: Int, _ rhs: Int) -> Int {
+    lhs - rhs
+}
+
+let customSubstracted = customMinus(20, 10)
