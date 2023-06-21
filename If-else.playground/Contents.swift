7317984521511a85d530bb4dd517f3805a9d6841
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 10:34:51 +0300

    🔨 refactor(If-else.playground): improve code readability by adding more descriptive if-else statements and comments

--- If-else.playground/Contents.swift
+++ If-else.playground/Contents.swift
@@ -1,3 +1,36 @@
 import Foundation
 
+let myName = "Rokas"
+let myAge = 20
+let yourName = "Tomas"
+let yourAge = 30
 
+if myName == "Rokas" {
+    "You name is  \(myName)"
+} else {
+    "Oopps I gueessed it wrong"
+}
+
+if myName == "Rokas" {
+    "Now iguessed it correctly"
+} else if myName == "Tomas" {
+    "Are you foo?"
+} else {
+    "Okay I give up"
+}
+
+if "Rokas" == myName {
+    "An uncommon way of doing this"
+}
+
+if myName == "Rokas" && myAge == 20 {
+    "Name is Rokas and age i  20"
+} else if myAge == 20 {
+    "I only guessed the name"
+} else {
+    "I do not know nothing"
+}
+
+if myAge == 20 || myName == "Rokas" {
+    "Either age is 20 or the name is Rokas"
+}
