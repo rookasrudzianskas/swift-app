48da96440fb0692b2484706133c00d09b2c31d64
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 13:47:41 +0300

    🚀 feat(Classes.playground): add Tesla and TeslaModelY classes to the playground 🐛 fix(Classes.playground): fix increaseAge method call on fooBar instance 🚀 feat(Classes.playground): add MyClass and myClosurel to the playground for testing purposes 🔥 chore(SWIFT_CRASH.xcworkspace): remove UserInterfaceState.xcuserstate binary file from git tracking

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -77,8 +77,70 @@ baz.increaseAge()
 baz.age
 
 
+class Tesla {
+    let manufacturer = "Tesla"
+    let model: String
+    let year: Int
+    
+    init() {
+        self.model = "X"
+        self.year = 2023
+    }
+    
+    init(
+        model: String,
+        year: Int
+    ) {
+        self.model = model
+        self.year = year
+    }
+    
+    convenience init(model: String) {
+        self.init(model: model, year: 2023)
+    }
+}
+
+class TeslaModelY: Tesla {
+    override init() {
+        super.init(model: "Y", year: 2023)
+    }
+//    super.init(model: "Y")
+}
+
+let modelY = TeslaModelY()
+modelY.model
+modelY.year
+modelY.manufacturer
+
+let fooBar = Person2(age: 20)
+fooBar.age
+
+func doSomething(with person: Person2) {
+    person.increaseAge()
+}
+
+doSomething(with: fooBar)
+fooBar.age
+
+class MyClass {
+    init() {
+        "Initialized"
+    }
+    
+    func doSomething() {
+        
+    }
+    
+    deinit {
+        "Deinitialized"
+    }
+}
 
 
+let myClosurel = {
+    let myClass = MyClass()
+    myClass.doSomething()
+}
 
-
+myClosurel()
 
