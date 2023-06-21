a9203a4166ca3e3dc9f3542a4cd48e2a3be569ba
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 12:32:24 +0300

    FIX

--- Enumerations.playground/Contents.swift
+++ Enumerations.playground/Contents.swift
@@ -1,3 +1,34 @@
 import Foundation
 
+enum Animals {
+    case cat, dog, rabbit
+}
 
+let cat = Animals.cat
+cat
+
+if cat == Animals.cat {
+    "This is a cat"
+} else if cat == Animals.dog {
+    "This is a dog"
+} else {
+    "This is something else"
+}
+
+switch cat {
+case .cat:
+    "This is cat"
+    break
+case .dog:
+    "This is a dog"
+    break
+default:
+    "This is soemthing else"
+}
+
+//func == (
+//    lhs: Animals,
+//    rhs: Int
+//) -> Bool {
+//    lhs == .cat && rhs == 3
+//}
