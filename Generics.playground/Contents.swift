73429aa0a22a082274f0508648504b8dc6b45990
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 16:59:45 +0300

    🐛 fix(Generics.playground): change the second argument of perform function to an integer to match the generic type 🔥 chore(SWIFT_CRASH.xcworkspace): remove UserInterfaceState.xcuserstate binary file from version control to avoid conflicts

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -5,7 +5,7 @@ func perform<N: Numeric>( _ op: (N, N) -> N, on lhs: N, and rhs: N) -> N {
     op(lhs, rhs)
 }
 
-let x = perform(+, on: 10.01, and: 20.03)
+let x = perform(+, on: 10.01, and: 20)
 x
 //
 //performInt(+, on: 10, and: 20)
