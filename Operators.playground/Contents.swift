d8c4d3b9548fa8f8ba2735bba781ddc51b7c7b1f
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 10:25:02 +0300

    🔨 chore(Operators.playground): add examples of unary prefix, unary postfix and binary infix operators 🔥 chore(SWIFT_CRASH.xcworkspace): remove UserInterfaceState.xcuserstate file from git tracking to avoid conflicts

--- Operators.playground/Contents.swift
+++ Operators.playground/Contents.swift
@@ -10,3 +10,37 @@ if myAge > yourAge {
 } else {
     "we are the same age"
 }
+
+let myMothersAge = myAge + 30
+let doubleMyAge = myAge * 2
+
+// 1 unary prefix
+
+let foo = !true
+
+// 2 unary postfix
+
+let name = Optional("Rokas")
+type(of: name)
+let unaryPostFix = name!
+type(of: unaryPostFix)
+// 3 binary infix
+
+let result = 1 + 2
+let names = "Foo" + " " + "bar"
+
+let age = 30
+let message: String
+if age >= 18 {
+    message = "You are an adult"
+} else {
+message = "You are not an adult yet"
+}
+
+let message2 = age >= 18
+? "You are an adult"
+: "You are not an adult"
+
+
+
+
