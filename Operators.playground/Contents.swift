9397d06ba3cce55f6f12a2b800ccffc71d70644b
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 10:11:10 +0300

    🎉 feat(Operators.playground): add playground file with age comparison using if-else statement 📝 chore(SWIFT_CRASH.xcworkspace): add Operators.playground to workspace 🐛 fix(SWIFT_CRASH.xcworkspace): remove unused xcscheme files for Operators.playground from xcschememanagement.plist

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let myAge = 22
+let yourAge = 20
+
+if myAge > yourAge {
+    "I am older than you"
+} else if myAge < yourAge {
+    "I am younger than you"
+} else {
+    "we are the same age"
+}
