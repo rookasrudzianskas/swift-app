8963140df77ebe89ac2ad8e75e0c635fc34c432c
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 18:48:32 +0300

    🔥 chore(Optionals.playground): comment out unused code and remove commented out code 🐛 fix(Optionals.playground): fix checkAge function to properly check if age is nil 🔒 chore(SWIFT_CRASH.xcworkspace): update UserInterfaceState.xcuserstate file to lock it and ignore it in version control

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -1,5 +1,36 @@
 import Foundation
 
-func multiplyByTwo(_ value: Int) -> Int {
-    value * 2
+//func multiplyByTwo(_ value: Int? = nil) -> Int {
+//    if let value {
+//        return value * 2
+//    } else {
+//        return 0
+//    }
+//}
+//
+//multiplyByTwo(nil)
+
+let age: Int? = nil
+
+if age != nil {
+    "Age is there"
+} else {
+    "Age is nil"
+}
+
+//if let age {
+//    "Age is there"
+//} else {
+//    "Not here"
+//}
+
+func checkAge() {
+    if age == nil {
+        "Age is nil"
+        return
+    }
+    guard age != nil else {
+        "Age is nil"
+        return
+    }
 }
