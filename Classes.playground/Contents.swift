140b2d56a2a922382a489fe74a980c3d485818e0
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 13:35:40 +0300

    🔨 refactor(Classes.playground): change Person struct to a class to be able to modify the age property 🔨 refactor(Classes.playground): add private access modifier to age property of Person2 class to improve encapsulation 🔥 chore(SWIFT_CRASH.xcworkspace): remove UserInterfaceState.xcuserstate file to declutter the workspace

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -1,3 +1,84 @@
 import Foundation
 
+//struct Person {
+//    var name: String
+//    var age: Int
+//}
+
+class Person {
+    var name: String
+    var age: Int
+    
+    init(
+        name: String,
+        age: Int
+    ) {
+        self.name = name
+        self.age = age
+        
+    }
+    
+    func increaseAge() {
+        self.age += 1
+    }
+}
+
+let foo = Person(name: "Foo", age: 20)
+
+foo.age
+foo.increaseAge()
+foo.age
+
+//foo = Person(name: "FOO", age: 20)
+
+foo.age
+let bar = foo
+bar.increaseAge()
+foo.age
+bar.age
+foo.age += 10
+foo.age
+
+if foo === bar {
+    "foo and bar point to the same memory"
+} else {
+    "The do not"
+}
+
+class Vehicle {
+    func goVroom() {
+        "Vrooooom"
+    }
+}
+
+class Car: Vehicle {
+    
+}
+
+let car = Car()
+car.goVroom()
+
+class Person2 {
+    private(set) var age: Int
+    init(age: Int) {
+        self.age = age
+    }
+    func increaseAge() {
+        self.age += 1
+    }
+}
+
+let baz = Person2(age: 20)
+baz.age
+//baz.age +=1
+
+
+baz.increaseAge()
+baz.age
+
+
+
+
+
+
 
