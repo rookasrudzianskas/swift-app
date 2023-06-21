4b36aba5cdfb92fdc592f6ce0f1880e4a28f96c2
Rokas <rookas.rudzenskas@gmail.com>
2023-06-21 20:07:37 +0300

    🐛 fix(Errorhandling.playground): fix getFullName() method to throw custom errors when first name, last name or both are nil 🔥 chore(SWIFT_CRASH.xcworkspace): remove UserInterfaceState.xcuserstate binary file from version control

--- Errorhandling.playground/Contents.swift
+++ Errorhandling.playground/Contents.swift
@@ -11,6 +11,56 @@ struct Person {
     }
     
     func getFullName() throws -> String {
-        ""
+        switch(firstName, lastName) {
+            case(.none, .none):
+                throw Errors.bothNamesAreNil
+            case(.none, .some):
+                throw Errors.firstNameIsNil
+            case (.some, .none):
+                throw Errors.lastNameIsNil
+            case let (.some(firstName), .some(lastName)):
+                return "\(firstName) \(lastName)"
+        }
+    
     }
 }
+
+let foo = Person(
+    firstName: nil,
+    lastName: nil
+)
+
+do {
+    let fullName = try foo.getFullName()
+} catch {
+    "Got an error = \(error)"
+}
+
+
+do {
+    let fullName = try foo.getFullName()
+    fullName
+} catch is Person.Errors {
+    "Got an error"
+}
+
+let bar = Person(
+    firstName: nil, lastName: nil
+)
+
+//do {
+//    let fullName = try bar.getFullName()
+//    fullName
+//} catch is Person.Errors.firstNameIsNil {
+//    "Got an error"
+//} catch is Person.Errors.lastNameIsNil {
+//    "Got an error"
+//
+//} catch is Person.Errors.bothNamesAreNil {
+//    "Got an error"
+//}  catch {
+//    "'Some other error'"
+//}
+
+
+
