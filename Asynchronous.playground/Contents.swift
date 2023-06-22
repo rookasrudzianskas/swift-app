4cfd2cb47c973fcfbd9f0da534ef4edfb08cd80f
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 11:23:27 +0300

    🔥 chore(Asynchronous.playground): remove commented out code and add support for indefinite execution 🔧 chore(Asynchronous.playground): enable automatic execution on source changes 🔥 chore(SWIFT_CRASH.xcworkspace): remove binary file that was accidentally committed

--- Asynchronous.playground/Contents.swift
+++ Asynchronous.playground/Contents.swift
@@ -1,3 +1,22 @@
 import Foundation
+import PlaygroundSupport
+import _Concurrency
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+//func calculateFullName(
+//    firstName: String,
+//    lastName: String
+//) async -> String {
+//    try? await Task.sleep(for: .seconds(1))
+//    return "This is working"
+//}
+
+//Task {
+//    let result1 = await calculateFullName(firstName: "Foo", lastName: "Bar")
+//    async let result2 = calculateFullName(firstName: "Rokas", lastName: "Rudzianskas")
+//    await result2
+//}
+
 
 
