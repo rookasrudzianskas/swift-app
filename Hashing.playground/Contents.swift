af723ac845d89fed26e115174b123d9dc4fd5be8
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 10:46:24 +0300

    🔨 refactor(Hashing.playground): add Equatable conformance to Person struct to enable comparison by id 🐛 fix(Hashing.playground): fix AnimalType enum, change whiskers to be of type cat instead of dog 🔥 chore(SWIFT_CRASH.xcworkspace): remove UserInterfaceState.xcuserstate binary file from git tracking

--- Hashing.playground/Contents.swift
+++ Hashing.playground/Contents.swift
@@ -1,3 +1,34 @@
 import Foundation
 
+struct Person: Equatable {
+    let id: String
+    let name: String
+}
+
+let foo1 = Person(id: "1", name: "Foo")
+let foo2 = Person(id: "2", name: "Bar")
+
+
+if foo1 == foo2 {
+    "Yes"
+} else {
+    "Not"
+}
+
+extension Person {
+    static func == (
+        lhs: Self,
+        rhs: Self
+    ) -> Bool {
+        lhs.id == rhs.id
+    }
+}
+
+enum AnimalType {
+    case dog(breed: String)
+    case cat(breed: String)
+}
+
+let dog = AnimalType.dog(breed: "Something")
+let whiskers = AnimalType.dog(breed: "None")
 
