cc509212daca57a3927f310395b3fa816d932daf
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 09:16:14 +0300

    🔨 chore(Collections.playground): add code to demonstrate basic array operations 🔨 chore(Collections.playground): enable executeOnSourceChanges to improve development experience 🔥 refactor(Collections.playground): remove unused import statement 🔥 refactor(SWIFT_CRASH.xcworkspace): remove binary files from version control to reduce repository size

--- Collections.playground/Contents.swift
+++ Collections.playground/Contents.swift
@@ -1,3 +1,12 @@
 import Foundation
 
+let numbers = [1, 2, 3, 4, 5]
 
+numbers.first
+numbers.last
+numbers.count
+numbers.map(-)
+
+var mutatingNumbers = [0, 1, 2]
+mutatingNumbers.append(7)
+mutatingNumbers.insert(3, at: 0)
