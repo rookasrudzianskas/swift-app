8864ee843d171b706c4e28b5ba6eeaee0009b015
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 11:16:07 +0300

    🔨 refactor(CustomOperators.playground): add custom operator to concatenate two optional strings and add custom operator to uppercase a string 🔨 refactor(CustomOperators.playground): add custom operator to add stars to the end of a string 🔨 refactor(CustomOperators.playground): add custom operator to concatenate two Person structs into a Family struct and add custom operator to add a Person to a Family struct

--- CustomOperators.playground/Contents.swift
+++ CustomOperators.playground/Contents.swift
@@ -1,3 +1,75 @@
 import Foundation
 
+let firstName: String? = "Foo"
+let lastName: String? = "Bar"
+let fullName = firstName + lastName
 
+func + (lhs: String?, rhs: String?) -> String? {
+    switch(lhs, rhs) {
+    case( .none, .none):
+        return nil
+    case let (.some(value), .none),
+        let ( .none, .some(value)):
+        return value
+    case  let ( .some(lhs), .some(rhs)):
+        return lhs + rhs
+    }
+}
+
+
+prefix operator ^
+prefix func ^ (value: String) -> String {
+    value.uppercased()
+}
+
+let lowercaseName = "foo Bar"
+let uppercaseName = ^lowercaseName
+
+
+postfix operator *
+postfix func * (value: String) -> String {
+    "*** \(value) ***"
+}
+
+let withStarts = lowercaseName*
+
+
+
+
+struct Person {
+    let name: String
+}
+
+struct Family {
+    let members: [Person]
+}
+
+let mom = Person(name: "Mom")
+let dad = Person(name: "Dad")
+let sis = Person(name: "SIs")
+let bro = Person(name: "Bro")
+
+
+func + (
+    lhs: Person,
+    rhs: Person
+) -> Family {
+    Family(members: [
+        lhs, rhs
+    ])
+}
+
+
+let family = mom + dad
+
+
+func + (
+    lhs: Family,
+    rhs: Person
+) -> Family {
+    var members = family.members
+    members.append(rhs)
+    return Family(members: members)
+}
+
+let familyWithBro = family + bro
