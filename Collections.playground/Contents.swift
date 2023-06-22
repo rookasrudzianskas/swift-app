691b2329d0811e05e84f045a8692eca37cfd0953
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 10:12:05 +0300

    🔥 chore(Collections.playground): remove commented out code and unused imports 🐛 fix(Collections.playground): fix typo in variable name 🚀 feat(Collections.playground): add support for filtering out nil values from an array of optionals 🚀 feat(Collections.playground): add support for creating a set of mixed type elements 🚀 feat(Collections.playground): add support for creating a set of custom struct elements 🚀 feat(Collections.playground): add support for custom hashable implementation in a struct 🔥 chore(Collections.playground): remove unused timeline.xctimeline and UserInterfaceState.xcuserstate files

--- Collections.playground/Contents.swift
+++ Collections.playground/Contents.swift
@@ -28,4 +28,110 @@ numbers.map {$0 * 2}
 
 numbers.filter { (value: Int) -> Bool in value >= 3}
 
-numbers.compactMap((value: Int) -> String? in value % 2 == 0 ? String(value) : nil)
+//numbers.compactMap((value: Int) -> String? in value % 2 == 0 ? String(value) : nil)
+
+let numbers2: [Int?] = [1, 2, nil ,4, 5]
+numbers2.count
+
+let notNils = numbers2.filter {(value: Int?) -> Bool in value != nil}
+
+//let notNils2 = numbers2.compactMap{{value: Int?} -> Int? in value)
+
+//notNils2
+
+//let numbers = [1, 2, 3, 4, 5]
+//numbers2.count
+
+let stuff1 = [
+    1,
+    "Hello",
+    2,
+    "World"
+] as [Any]
+
+stuff1.count
+
+
+let stuff2: [Any] = [
+    1,
+    "Hello",
+    2,
+    "World"
+]
+
+stuff2.count
+
+let uniqueNumbers = Set([1, 2, 3, 4, 5])
+
+uniqueNumbers.count
+uniqueNumbers.map(-)
+
+let myNumbers = Set([1, 2, 3, nil, 5])
+//let notNilNumbers = Set(
+//    myNumbers.compactMap({ <#Int?#> in
+//        <#code#>
+//    })
+//)
+
+let stuff3: Set<AnyHashable> = [
+    1, 2, 3, "ROkas"
+]
+
+stuff3.count
+
+let intsInStuff1 = stuff1.compactMap {(value: Any) -> Int? in value as? Int}
+
+let intsInStuff2 = stuff1.compactMap {(value: Any) -> String? in value as? String}
+
+let intsInStuff3 = stuff3.compactMap {(value: AnyHashable) -> Int? in value as? Int}
+
+type(of: intsInStuff3)
+
+struct Person: Hashable {
+    let id: UUID
+    let name: String
+    let age: Int
+}
+
+let fooId = UUID()
+let foo = Person(id: fooId, name: "Foo", age: 20)
+let bar = Person(id: fooId, name: "Bar", age: 30)
+
+
+let people: Set<Person> = [foo, bar]
+
+people.count
+
+
+struct Person2: Hashable {
+    let id: UUID
+    let name: String
+    let age: Int
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (
+        lhs: Self,
+        rhs: Self
+    ) -> Bool {
+        lhs.id == rhs.id
+    }
+}
+
+
+let bazId = UUID()
+
+let baz = Person2(id: bazId, name: "baz", age: 20)
+
+let qk = Person2(id: bazId, name: "Quic", age: 30)
+
+if baz == qk {
+    "Same"
+}
+
+let poeple2 = Set([baz, qk])
+people2
+
+
