e4fa4646e1323244fba34d14793ad59959c260d2
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 09:51:45 +0300

    🔨 refactor(Collections.playground): remove unused code and add more examples of collection operations 📝 docs(Collections.playground): add more examples of collection operations to improve code readability and understanding

--- Collections.playground/Contents.swift
+++ Collections.playground/Contents.swift
@@ -10,3 +10,22 @@ numbers.map(-)
 var mutatingNumbers = [0, 1, 2]
 mutatingNumbers.append(7)
 mutatingNumbers.insert(3, at: 0)
+mutatingNumbers.insert(contentsOf: [1, 2], at: 0)
+
+for value in numbers {
+    value
+}
+
+for value in numbers where value % 2 == 0 {
+    value
+}
+
+numbers.map {(value: Int) -> String in String(value * 2)}
+
+let values = numbers.map {(value: Int) -> Int in value * 2}
+
+numbers.map {$0 * 2}
+
+numbers.filter { (value: Int) -> Bool in value >= 3}
+
+numbers.compactMap((value: Int) -> String? in value % 2 == 0 ? String(value) : nil)
